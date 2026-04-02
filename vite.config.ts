ae745845e87cdd9d79129be3525d259c595a3787
Ahmed Elkousha <elkoushaservice@gmail.com>
2026-04-02 22:47:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 const port = process.env.PORT ? Number(process.env.PORT) : 5173;
-const basePath = process.env.BASE_PATH || "/";
+const basePath = "/";
 
 export default defineConfig({
   base: basePath,
