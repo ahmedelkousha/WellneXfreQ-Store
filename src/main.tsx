a684ee3f246ef227f30624e9aadda0bbcbd745c5
ahmedelkousha <elkoushaservice@gmail.com>
2026-04-15 08:29:48 +0200

    tags in html

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,5 @@ import { createRoot } from "react-dom/client";
 import "./i18n";
 import App from "./App";
 import "./index.css";
-import TagManager from 'react-gtm-module';
-
-const tagManagerArgs = {
-    gtmId: import.meta.env.PPV_GTM_ID
-};
-
-TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById("root")!).render(<App />);
