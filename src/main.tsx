7b5c14e367cca2ef061624fd407cd4bf5847b836
ahmedelkousha <elkoushaservice@gmail.com>
2026-04-15 07:37:08 +0200

    gtm gtm-react-module

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./i18n";
 import App from "./App";
 import "./index.css";
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+    gtmId: import.meta.env.VITE_GTM_ID
+};
+
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById("root")!).render(<App />);
